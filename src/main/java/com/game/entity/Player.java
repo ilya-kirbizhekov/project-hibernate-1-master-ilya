f63be18cbbaff82843de7f813f1ddadf7bf2a031
Ilya Kirbizhekov <ilyoniss@mail.ru>
2023-09-10 15:57:35 +1000

    configs

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -6,6 +6,7 @@ import java.util.Date;
 
 @Entity
 @Table(schema = "rpg", name = "player")
+@NamedQuery(name = "player_getAllCount",query = "select count(p) from Player p")
 public class Player {
     @Id
     @Column(nullable = false)
--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,9 +1,12 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
+import org.hibernate.query.NativeQuery;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PreDestroy;
@@ -20,11 +23,11 @@ public class PlayerRepositoryDB implements IPlayerRepository {
     public PlayerRepositoryDB() {
 
         Properties properties = new Properties();
-        properties.put(Environment.URL, "org.hibernate.dialect.MySQL8Dialect");
+        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/rpg");
         properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-        properties.put(Environment.USER, "jdbc:mysql://localhost:3306/rpg");
-        properties.put(Environment.PASS, "root");
-        properties.put(Environment.DIALECT, "p@ssw0rd");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "p@ssw0rd");
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
         properties.put(Environment.HBM2DDL_AUTO, "update");
 
         sessionFactory = new Configuration()
@@ -36,17 +39,33 @@ public class PlayerRepositoryDB implements IPlayerRepository {
 
     @Override
     public List<Player> getAll(int pageNumber, int pageSize) {
-        return null;
+
+        try (Session session = sessionFactory.openSession()) {
+           NativeQuery<Player> query = session.createNativeQuery("SELECT * FROM rpg.player", Player.class);
+           query.setFirstResult(pageNumber*pageSize);
+           query.setMaxResults(pageSize);
+           return query.list();
+
+        }
+
     }
 
     @Override
     public int getAllCount() {
-        return 0;
+        try(Session session = sessionFactory.openSession()) {
+           Query<Long> query = session.createNamedQuery("player_getAllCount", Long.class);
+           return Math.toIntExact(query.uniqueResult());
+        }
     }
 
     @Override
     public Player save(Player player) {
-        return null;
+
+        try(Session session = sessionFactory.openSession()) {
+           session.save(player);
+           return player;
+        }
+
     }
 
     @Override
@@ -67,5 +86,6 @@ public class PlayerRepositoryDB implements IPlayerRepository {
     @PreDestroy
     public void beforeStop() {
 
+        sessionFactory.close();
     }
 }
