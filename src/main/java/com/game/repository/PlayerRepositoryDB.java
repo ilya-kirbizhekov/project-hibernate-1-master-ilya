c0182077db406d22fb2ad93fcebc7a58f1708a35
Ilya Kirbizhekov <ilyoniss@mail.ru>
2023-09-10 15:16:34 +1000

    configs

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,16 +1,36 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
 
+    private final SessionFactory sessionFactory;
+
+
     public PlayerRepositoryDB() {
+
+        Properties properties = new Properties();
+        properties.put(Environment.URL, "org.hibernate.dialect.MySQL8Dialect");
+        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        properties.put(Environment.USER, "jdbc:mysql://localhost:3306/rpg");
+        properties.put(Environment.PASS, "root");
+        properties.put(Environment.DIALECT, "p@ssw0rd");
+        properties.put(Environment.HBM2DDL_AUTO, "update");
+
+        sessionFactory = new Configuration()
+                .addAnnotatedClass(Player.class)
+                .addProperties(properties)
+                .buildSessionFactory();
 
     }
 
