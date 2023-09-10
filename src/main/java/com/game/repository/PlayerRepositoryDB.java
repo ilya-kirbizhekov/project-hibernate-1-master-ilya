aa483a978942616a859759b51f30f106907bb43c
Ilya Kirbizhekov <ilyoniss@mail.ru>
2023-09-10 16:50:11 +1000

    configs

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -24,8 +24,8 @@ public class PlayerRepositoryDB implements IPlayerRepository {
     public PlayerRepositoryDB() {
 
         Properties properties = new Properties();
-        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/rpg");
-        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/rpg");
+        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
         properties.put(Environment.USER, "root");
         properties.put(Environment.PASS, "p@ssw0rd");
         properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
